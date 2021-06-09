4f08b8e2bba7dd1bba1485aa732f711faa9749f5
powerycy <36032407+powerycy@users.noreply.github.com>
2021-06-09 14:31:33 +0800

    Delete ExtractionEntities directory

--- ExtractionEntities/metrics.py
+++
@@ -1,4 +0,0 @@
-import torch
-def global_pointer_f1_score(y_true, y_pred):
-    y_pred = torch.greater(y_pred, 0)
-    return torch.sum(y_true * y_pred),torch.sum(y_true + y_pred)
--- ExtractionEntities/run_globalpointer.sh
+++
@@ -1,2 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0,1
-python -m torch.distributed.launch --nproc_per_node=2 globalpointer_train.py
